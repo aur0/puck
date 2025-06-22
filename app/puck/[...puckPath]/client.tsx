4fabe096bb936fd290bab4c1147ec438e094c492
James Warren <ted3@protonmail.com>
2025-06-22 11:25:08 +0000

    Your commit message here

--- app/puck/[...puckPath]/client.tsx
+++ app/puck/[...puckPath]/client.tsx
@@ -3,15 +3,12 @@
 import type { Data } from "@measured/puck";
 import { Puck } from "@measured/puck";
 import config from "../../../puck.config";
-import { useEffect } from "react";
-
 
 export function Client({ path, data }: { path: string; data: Partial<Data> }) {
   return (
     <Puck
       config={config}
       data={data}
-      //overrides={overrides}
       onPublish={async (data) => {
         await fetch("/puck/api", {
           method: "post",
