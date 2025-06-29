9cf723f617b10c881f2387c2ecece0d354741493
James Warren <ted3@protonmail.com>
2025-06-29 18:44:54 +0000

    cache 24 hours

--- app/[...puckPath]/page.tsx
+++ app/[...puckPath]/page.tsx
@@ -40,5 +40,6 @@ export default async function Page({
   return <Client data={data} />;
 }
 
-// Force Next.js to produce static pages
+// Force Next.js to produce static pages with 24 hour cache
 export const dynamic = "force-static";
+export const revalidate = 86400; // 24 hours
